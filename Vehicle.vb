54dde865165395f329ef03aea5a7e560a62da067
Antipas Zemba <antipasnzeba@gmail.com>
2026-04-04 14:01:16 -0700

    added vehicle module

---
+++ Vehicle.vb
@@ -0,0 +1,10 @@
+﻿Public Class Vehicle
+    Public Property Make As String
+    Public Property Model As String
+    Public Property Year As Integer
+    Public Property Price As Decimal
+
+    Public Overrides Function ToString() As String
+        Return $"{Year} {Make} {Model} - {Price:C}"
+    End Function
+End Class
