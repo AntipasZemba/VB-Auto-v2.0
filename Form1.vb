d6e5b754a8658fa0c2111c38ebda9f3cb9ad5ee0
Antipas Zemba <antipasnzeba@gmail.com>
2026-04-04 14:06:30 -0700

    Add constants for pricing and tax rate
    Added constants for tax rate and item prices.

--- Form1.vb
+++ Form1.vb
@@ -3,4 +3,11 @@ imports System.Drawing.Printing
 
 Public Class Form1
 
+  ' Constants
+  Private Constant Tax_Rate As Decimal = 0.08
+  Private Constant Sound_Price as Decimal = 500
+  Private Const Leather_Price As Decimal = 1000
+  Private Const Nav_Price As Decimal = 1500
+
+  
 End Class
