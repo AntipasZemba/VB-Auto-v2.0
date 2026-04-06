37816a88facd3790630fbd8e68f1f7b14373de11
Antipas Zemba <antipasnzeba@gmail.com>
2026-04-06 06:44:15 -0700

    Implement vehicle update feature
    Added functionality to update selected vehicle details.

--- Form1.vb
+++ Form1.vb
@@ -55,6 +55,26 @@ Public Class Form1
     ' Add to list and refresh display
     inventory.Add(v)
     RefreshInventory()
-End Sub
+  End Sub
 
+  ' Updates selected vehicle with new values
+  Private Sub btnUpdateVehicle_Click(sender As Object, e As EventArgs) Handles btnUpdateVehicle.Click
+    Dim selected As Vehicle = CType(lstInventory.SelectedItem, Vehicle)
+
+    ' Ensure a vehicle is selected
+    If selected Is Nothing Then
+        MessageBox.Show("Select a vehicle to update")
+        Exit Sub
+    End If
+
+    ' update properties
+    selected.Make = txtMake.Text
+    selected.Model = txtModel.Text
+    Integer.TryParse(txtYear.Text, selected.Year)
+    Decimal.TryParse(txtPrice.Text, selected.Price)
+
+    ' Refresh Display
+    RefreshInventory()
+  End Sub
+      
 End Class
